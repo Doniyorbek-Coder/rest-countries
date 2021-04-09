39471d7261332863ada2059b87e63ed9e4c2ec49
Doniyor-Coder <asqaraliyev1559099@gmail.com>
2021-04-09 22:41:06 +0500

    some codes updated

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-let theme = document.getElementById('theme-mode')
-let n = 0
+let theme = document.getElementById('theme-mode');
+let button = document.getElementById('button');
+let n = 0;
 function changeTheme() {
   if (n == 0) {
-    theme.innerHTML = 'Light Mode'
-    document.documentElement.style.setProperty('--black', '#ffffff')
-    document.documentElement.style.setProperty('--body-bg', '#202c36')
-    document.documentElement.style.setProperty('--white', '#2B3844')
+    button.style.backgroundImage='url(../images/searchblack.png)';
+    theme.innerHTML = 'Light Mode';
+    document.documentElement.style.setProperty('--black', '#ffffff');
+    document.documentElement.style.setProperty('--body-bg', '#202c36');
+    document.documentElement.style.setProperty('--white', '#2B3844');
     n = 1
   } else {
-    theme.innerHTML = 'Dark Mode'
-    document.documentElement.style.setProperty('--black', '#191919')
-    document.documentElement.style.setProperty('--body-bg', '#f2f2f2')
-    document.documentElement.style.setProperty('--white', '#ffffff')
+    button.style.backgroundImage='url(../images/search.png)';
+    theme.innerHTML = 'Dark Mode';
+    document.documentElement.style.setProperty('--black', '#191919');
+    document.documentElement.style.setProperty('--body-bg', '#f2f2f2');
+    document.documentElement.style.setProperty('--white', '#ffffff');
     n = 0
   }
 }
